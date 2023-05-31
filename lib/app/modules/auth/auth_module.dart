96e48fcd2bd40cd633cc075e556403ac7a2b1f21
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-05-31 15:02:36 -0300

    initializing bloc in login module

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -1,5 +1,5 @@
-import 'package:friendly_payment/app/modules/auth/login/login_controller.dart';
-import 'package:friendly_payment/app/modules/auth/register/register_controller.dart';
+import 'package:friendly_payment/app/modules/auth/login/controller/login_controller.dart';
+import 'package:friendly_payment/app/modules/auth/register/controller/register_controller.dart';
 import 'package:friendly_payment/app/modules/auth/register/register_page.dart';
 import 'package:provider/provider.dart';
 
---
+++ lib/app/modules/auth/login/controller/login_state.dart
@@ -0,0 +1,3 @@
+enum LoginStatus { initial, loading, failure }
+
+class LoginState {}
